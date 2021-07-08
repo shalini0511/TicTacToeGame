31abf9f1579e43b733a060a3ed9e5f08f92aa6e2
shalini <shalini.v@tvsnext.io>
2021-07-08 13:31:04 +0530

    [shalini]Displaying

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -30,9 +30,26 @@ namespace TicTacToeGame
         
            
         
-        public static void Display()
+        public static void  Display()
         {
-            Console.WriteLine(board);
+            char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+
+            Console.WriteLine("     |     |      ");
         }
         
         static void Main(string[] args)
@@ -41,6 +58,7 @@ namespace TicTacToeGame
             char[] board = Initializing();
             char letter = Choosing();
             Console.WriteLine(letter);
+            Program.Display();
             Console.ReadLine();
         }
     }
