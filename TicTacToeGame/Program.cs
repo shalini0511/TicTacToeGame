ab718a2462cdb13dcc717baf25e9ca2228cd2282
shalini <shalini.v@tvsnext.io>
2021-07-08 11:35:54 +0530

    [shalini]Initializing grid for tic tac toe

---
+++ TicTacToeGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    class Program
+    {
+        public static void Initializing()
+        {
+            char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+
+            Console.WriteLine("     |     |      ");
+            
+        }
+        static void Main(string[] args)
+        {
+            Program.Initializing();
+            Console.ReadLine();
+        }
+    }
+}
